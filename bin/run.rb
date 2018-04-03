a387f3f3edf5a14f07261e8aaee457138b611431
briecodes <hello@brie.work>
2018-04-03 19:52:04 -0400

    Fixed RUN file.

--- bin/run.rb
+++ bin/run.rb
@@ -4,8 +4,5 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-main menu
 character = get_character_from_user
-planet = get_planet_from_user
-movie = get_movie_from_user
-show_character_info(character)
+show_character_movies(character)
