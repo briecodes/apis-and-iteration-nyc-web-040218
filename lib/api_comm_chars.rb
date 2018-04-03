da099b6c7c5cee9675cc1509fd321444b5798224
Matthew Talbot <matthewtalbot@Matthews-MacBook-Pro.local>
2018-04-03 18:02:12 -0400

    6pm tues

--- lib/api_comm_chars.rb
+++ lib/api_comm_chars.rb
@@ -1,18 +1,17 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
 def get_character_info_from_api(character)
-  #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  movie_hash = character_hash["results"].map { |char|
-    if char["name"].downcase.include?(character.downcase)
-      char["films"]
-    end
-  }.compact.flatten
-  return get_movie_info(movie_hash)
+  char_hash = JSON.parse(all_characters)
+  char_hash["results"].map do |hash|
+    hash
 end
 
 def get_char_info(char)
   get_character_info_from_api.each do |char_hash|
-    if p_hash[“name”].downcase.include?(planet.downcase)
+    if char_hash["name"].downcase.include?(char.downcase)
       puts "#{char_hash["name"} is a #{get_char_species(char_hash["species"])} #{char_hash["gender"]} with #{char_hash["hair_color"]} hair, #{char_hash["eye_color"]} eyes, and #{char_hash["skin_color"]} skin who is #{char_hash["height"]}cm tall and weighs #{char_hash["mass"]}kg.
 
       They appear in the following films:
@@ -26,6 +25,9 @@ def get_char_species(species_array)
     species = RestClient.get(url)
     species_parsed = JSON.parse(species)
   end
+  species_hash.map do |hash|
+    hash["species"]
+  end
 end
 
 
@@ -37,6 +39,11 @@ def get_char_movies(url_array)
   movie_info.map do |hash|
     hash["title"]
   end
+end
+
+def list_chars
+  chars_array = get_character_info_from_api.map {|hash| hash["name"]}
+  puts chars_array.sort.join("\n")
 end
 
 binding.pry
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -40,38 +40,7 @@ def get_character_info_from_api(character)
   return get_movie_info(movie_hash)
 end
 
-def get_char_info(char)
-  get_character_info_from_api.each do |char_hash|
-    if p_hash[“name”].downcase.include?(planet.downcase)
-      puts "#{char_hash["name"} is a #{get_char_species(char_hash["species"])} #{char_hash["gender"]} with #{char_hash["hair_color"]} hair, #{char_hash["eye_color"]} eyes, and #{char_hash["skin_color"]} skin who is #{char_hash["height"]}cm tall and weighs #{char_hash["mass"]}kg.
-
-      They appear in the following films:
-      #{get_char_movies(char_hash["films"]).join("\n")}"
-    end
-  end
-end
-
-def get_char_species(species_array)
-  species_hash = species_array.map do |url|
-    species = RestClient.get(url)
-    species_parsed = JSON.parse(species)
-  end
-end
-
-
-def get_char_movies(url_array)
-  movie_info = url_array.each do |urls|
-    fetch_films = RestClient.get(urls)
-    films_parsed = JSON.parse(fetch_films)
-  end
-  movie_info.map do |hash|
-    hash["title"]
-  end
-end
-
-binding.pry
-
-=begin def parse_character_movies(films_hash)
+def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
   if films_hash.any?
     unsorted = []
@@ -82,7 +51,7 @@ binding.pry
   else
     puts "Sorry, that character doesn’t exist in the Star Wars Universe."
   end
-end =end
+end
 
 def show_character_info(character)
   films_hash = get_character_movies_from_api(character)
